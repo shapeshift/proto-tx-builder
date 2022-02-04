2fd0eb0d815af0fb010c2312a2d46b6debb8b37a
highlander <bithighlander@gmail.com>
2022-02-04 13:46:50 -0700

    init

---
+++ src/index.test.ts
@@ -0,0 +1,253 @@
+import {sign} from '../src/index';
+let {
+    SLIP_44_BY_LONG,
+    bip32ToAddressNList
+} = require('@pioneer-platform/pioneer-coins')
+import { mnemonicToSeed, validateMnemonic } from "bip39";
+import * as bitcoin from "@bithighlander/bitcoin-cash-js-lib";
+import { default as util } from "./util";
+import { getNetwork } from "./networks";
+const fs = require("fs");
+const log = require("@pioneer-platform/loggerdog")()
+
+const supported_assets = [
+    'cosmos',
+    'osmosis',
+    // 'thorchain',
+    // 'terra',
+    // 'kava',
+    // 'secret',
+
+]
+
+let prefixs = {
+    'osmosis':'osmo',
+    'cosmos':'cosmos'
+}
+
+const REFERENCE_SEED = "alcohol woman abuse must during monitor noble actual mixed trade anger aisle"
+
+describe('signs Tendermint transactions', async function() {
+    let tag = ' | sign | '
+    let asset = 'osmosis'
+    for(let i = 0; i < supported_assets.length; i++){
+        let asset = supported_assets[i]
+        console.log("ASSET: ",asset)
+
+        //Osmosis only
+        // if(asset === 'osmosis'){
+        //     it('signs a mainnet '+asset+' reference swap transaction', async function() {
+        //         //get reference data
+        //         let referenceTx = fs.readFileSync('./src/reference-data/defi/tx01.mainnet.'+asset+'.swap.json');
+        //         referenceTx = JSON.parse(referenceTx.toString())
+        //
+        //         let referenceTxSigned = fs.readFileSync('./src/reference-data/staking/tx01.mainnet.'+asset+'.swap.signed.json');
+        //         referenceTxSigned = JSON.parse(referenceTxSigned.toString())
+        //
+        //         // log.info(tag,"referenceTx: ",referenceTx)
+        //         // log.info(tag,"referenceTxSigned: ",referenceTxSigned)
+        //         expect(referenceTx).toBeTruthy();
+        //         expect(referenceTxSigned).toBeTruthy();
+        //
+        //         const network = getNetwork(asset);
+        //         const wallet = bitcoin.bip32.fromSeed(await mnemonicToSeed(REFERENCE_SEED), network);
+        //
+        //         const masterPath = bip32ToAddressNList("m/44'/"+SLIP_44_BY_LONG[asset].toString()+"'/0'/0/0")
+        //         log.info(tag,"masterPath: ",masterPath)
+        //         // const keyPair = util.getKeyPair(wallet, masterPath, asset);
+        //         // log.info(tag,"keyPair: ",keyPair)
+        //         // @ts-ignore
+        //         let prefix = prefixs[asset]
+        //         const result = await sign(referenceTx, REFERENCE_SEED, referenceTx.sequence, referenceTx.account_number, referenceTx.chain_id, prefix);
+        //         log.info(tag,"result: ",result)
+        //
+        //
+        //         expect(result.serialized).toBe(referenceTxSigned.serialized);
+        //         expect(result.signatures[0]).toBe(referenceTxSigned.signatures[0]);
+        //     });
+        // }
+
+        //IBC
+        // it('signs a mainnet '+asset+' reference IBC transfer transaction', async function() {
+        //     //get reference data
+        //     let referenceTx = fs.readFileSync('./src/reference-data/ibc/tx01.mainnet.'+asset+'.ibc.transfer.json');
+        //     referenceTx = JSON.parse(referenceTx.toString())
+        //
+        //     let referenceTxSigned = fs.readFileSync('./src/reference-data/ibc/tx01.mainnet.'+asset+'.ibc.transfer.signed.json');
+        //     referenceTxSigned = JSON.parse(referenceTxSigned.toString())
+        //
+        //     // log.info(tag,"referenceTx: ",referenceTx)
+        //     // log.info(tag,"referenceTxSigned: ",referenceTxSigned)
+        //     expect(referenceTx).toBeTruthy();
+        //     expect(referenceTxSigned).toBeTruthy();
+        //
+        //     const network = getNetwork(asset);
+        //     const wallet = bitcoin.bip32.fromSeed(await mnemonicToSeed(REFERENCE_SEED), network);
+        //
+        //     const masterPath = bip32ToAddressNList("m/44'/"+SLIP_44_BY_LONG[asset].toString()+"'/0'/0/0")
+        //     log.info(tag,"masterPath: ",masterPath)
+        //     // const keyPair = util.getKeyPair(wallet, masterPath, asset);
+        //     // log.info(tag,"keyPair: ",keyPair)
+        //     // @ts-ignore
+        //     let prefix = prefixs[asset]
+        //     const result = await sign(referenceTx, REFERENCE_SEED, referenceTx.sequence, referenceTx.account_number, referenceTx.chain_id, prefix);
+        //     log.info(tag,"result: ",result)
+        //
+        //
+        //     expect(result.serialized).toBe(referenceTxSigned.serialized);
+        //     expect(result.signatures[0]).toBe(referenceTxSigned.signatures[0]);
+        // });
+
+        it('signs a mainnet '+asset+' reference undelegate transaction', async function() {
+            //get reference data
+            let referenceTx = fs.readFileSync('./src/reference-data/staking/tx01.mainnet.'+asset+'.undelegate.json');
+            referenceTx = JSON.parse(referenceTx.toString())
+
+            let referenceTxSigned = fs.readFileSync('./src/reference-data/staking/tx01.mainnet.'+asset+'.undelegate.signed.json');
+            referenceTxSigned = JSON.parse(referenceTxSigned.toString())
+
+            // log.info(tag,"referenceTx: ",referenceTx)
+            // log.info(tag,"referenceTxSigned: ",referenceTxSigned)
+            expect(referenceTx).toBeTruthy();
+            expect(referenceTxSigned).toBeTruthy();
+
+            const network = getNetwork(asset);
+            const wallet = bitcoin.bip32.fromSeed(await mnemonicToSeed(REFERENCE_SEED), network);
+
+            const masterPath = bip32ToAddressNList("m/44'/"+SLIP_44_BY_LONG[asset].toString()+"'/0'/0/0")
+            log.info(tag,"masterPath: ",masterPath)
+            // const keyPair = util.getKeyPair(wallet, masterPath, asset);
+            // log.info(tag,"keyPair: ",keyPair)
+            // @ts-ignore
+            let prefix = prefixs[asset]
+            const result = await sign(referenceTx, REFERENCE_SEED, referenceTx.sequence, referenceTx.account_number, referenceTx.chain_id, prefix);
+            log.info(tag,"result: ",result)
+
+
+            expect(result.serialized).toBe(referenceTxSigned.serialized);
+            expect(result.signatures[0]).toBe(referenceTxSigned.signatures[0]);
+        });
+
+        it('signs a mainnet '+asset+' reference claim rewards transaction', async function() {
+            //get reference data
+            let referenceTx = fs.readFileSync('./src/reference-data/staking/tx01.mainnet.'+asset+'.rewards.json');
+            referenceTx = JSON.parse(referenceTx.toString())
+
+            let referenceTxSigned = fs.readFileSync('./src/reference-data/staking/tx01.mainnet.'+asset+'.rewards.signed.json');
+            referenceTxSigned = JSON.parse(referenceTxSigned.toString())
+
+            // log.info(tag,"referenceTx: ",referenceTx)
+            // log.info(tag,"referenceTxSigned: ",referenceTxSigned)
+            expect(referenceTx).toBeTruthy();
+            expect(referenceTxSigned).toBeTruthy();
+
+            const network = getNetwork(asset);
+            const wallet = bitcoin.bip32.fromSeed(await mnemonicToSeed(REFERENCE_SEED), network);
+
+            const masterPath = bip32ToAddressNList("m/44'/"+SLIP_44_BY_LONG[asset].toString()+"'/0'/0/0")
+            log.info(tag,"masterPath: ",masterPath)
+            // const keyPair = util.getKeyPair(wallet, masterPath, asset);
+            // log.info(tag,"keyPair: ",keyPair)
+            // @ts-ignore
+            let prefix = prefixs[asset]
+            const result = await sign(referenceTx, REFERENCE_SEED, referenceTx.sequence, referenceTx.account_number, referenceTx.chain_id, prefix);
+            log.info(tag,"result: ",result)
+
+
+            expect(result.serialized).toBe(referenceTxSigned.serialized);
+            expect(result.signatures[0]).toBe(referenceTxSigned.signatures[0]);
+        });
+
+        it('signs a mainnet '+asset+' reference redelegate transaction', async function() {
+            //get reference data
+            let referenceTx = fs.readFileSync('./src/reference-data/staking/tx01.mainnet.'+asset+'.redelegate.json');
+            referenceTx = JSON.parse(referenceTx.toString())
+
+            let referenceTxSigned = fs.readFileSync('./src/reference-data/staking/tx01.mainnet.'+asset+'.redelegate.signed.json');
+            referenceTxSigned = JSON.parse(referenceTxSigned.toString())
+
+            // log.info(tag,"referenceTx: ",referenceTx)
+            // log.info(tag,"referenceTxSigned: ",referenceTxSigned)
+            expect(referenceTx).toBeTruthy();
+            expect(referenceTxSigned).toBeTruthy();
+
+            const network = getNetwork(asset);
+            const wallet = bitcoin.bip32.fromSeed(await mnemonicToSeed(REFERENCE_SEED), network);
+
+            const masterPath = bip32ToAddressNList("m/44'/"+SLIP_44_BY_LONG[asset].toString()+"'/0'/0/0")
+            log.info(tag,"masterPath: ",masterPath)
+            // const keyPair = util.getKeyPair(wallet, masterPath, asset);
+            // log.info(tag,"keyPair: ",keyPair)
+            // @ts-ignore
+            let prefix = prefixs[asset]
+            const result = await sign(referenceTx, REFERENCE_SEED, referenceTx.sequence, referenceTx.account_number, referenceTx.chain_id, prefix);
+            log.info(tag,"result: ",result)
+
+
+            expect(result.serialized).toBe(referenceTxSigned.serialized);
+            expect(result.signatures[0]).toBe(referenceTxSigned.signatures[0]);
+        });
+
+        it('signs a mainnet '+asset+' reference delegate transaction', async function() {
+            //get reference data
+            let referenceTx = fs.readFileSync('./src/reference-data/staking/tx01.mainnet.'+asset+'.delegate.json');
+            referenceTx = JSON.parse(referenceTx.toString())
+
+            let referenceTxSigned = fs.readFileSync('./src/reference-data/staking/tx01.mainnet.'+asset+'.delegate.signed.json');
+            referenceTxSigned = JSON.parse(referenceTxSigned.toString())
+
+            // log.info(tag,"referenceTx: ",referenceTx)
+            // log.info(tag,"referenceTxSigned: ",referenceTxSigned)
+            expect(referenceTx).toBeTruthy();
+            expect(referenceTxSigned).toBeTruthy();
+
+            const network = getNetwork(asset);
+            const wallet = bitcoin.bip32.fromSeed(await mnemonicToSeed(REFERENCE_SEED), network);
+
+            const masterPath = bip32ToAddressNList("m/44'/"+SLIP_44_BY_LONG[asset].toString()+"'/0'/0/0")
+            log.info(tag,"masterPath: ",masterPath)
+            // const keyPair = util.getKeyPair(wallet, masterPath, asset);
+            // log.info(tag,"keyPair: ",keyPair)
+            // @ts-ignore
+            let prefix = prefixs[asset]
+            const result = await sign(referenceTx, REFERENCE_SEED, referenceTx.sequence, referenceTx.account_number, referenceTx.chain_id, prefix);
+            log.info(tag,"result: ",result)
+
+
+            expect(result.serialized).toBe(referenceTxSigned.serialized);
+            expect(result.signatures[0]).toBe(referenceTxSigned.signatures[0]);
+        });
+
+        it('signs a mainnet '+asset+' reference transfer transaction', async function() {
+            //get reference data
+            let referenceTx = fs.readFileSync('./src/reference-data/transfers/tx01.mainnet.'+asset+'.json');
+            referenceTx = JSON.parse(referenceTx.toString())
+
+            let referenceTxSigned = fs.readFileSync('./src/reference-data/transfers/tx01.mainnet.'+asset+'.signed.json');
+            referenceTxSigned = JSON.parse(referenceTxSigned.toString())
+
+            log.info(tag,"referenceTx: ",referenceTx)
+            log.info(tag,"referenceTxSigned: ",referenceTxSigned)
+            expect(referenceTx).toBeTruthy();
+            expect(referenceTxSigned).toBeTruthy();
+
+            const network = getNetwork(asset);
+            const wallet = bitcoin.bip32.fromSeed(await mnemonicToSeed(REFERENCE_SEED), network);
+
+            const masterPath = bip32ToAddressNList("m/44'/"+SLIP_44_BY_LONG[asset].toString()+"'/0'/0/0")
+            log.info(tag,"masterPath: ",masterPath)
+            // const keyPair = util.getKeyPair(wallet, masterPath, asset);
+            // log.info(tag,"keyPair: ",keyPair)
+            // @ts-ignore
+            let prefix = prefixs[asset]
+            const result = await sign(referenceTx, REFERENCE_SEED, referenceTx.sequence, referenceTx.account_number, referenceTx.chain_id, prefix);
+            log.info(tag,"result: ",result)
+
+
+            expect(result.serialized).toBe(referenceTxSigned.serialized);
+            expect(result.signatures[0]).toBe(referenceTxSigned.signatures[0]);
+        });
+    }
+
+
+});
---
+++ src/index.ts
@@ -0,0 +1,249 @@
+
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { SigningStargateClient, defaultRegistryTypes as defaultStargateTypes } from "@cosmjs/stargate";
+import { coins, Registry } from "@cosmjs/proto-signing";
+import { TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import { HdPath, Slip10RawIndex } from "@cosmjs/crypto";
+// import {osmosis} from "@pioneer-platform/osmosis-tx-codecs"
+
+export async function sign(jsonTx:any, seed:string, sequence:string, account_number:string, chain_id:string, prefix:string) {
+    let tag = " | sign | ";
+    try {
+        //TODO dont assume mnemonic
+        let path = makeCosmoshubPath(0)
+        // const myRegistry = new Registry(defaultStargateTypes);
+        const wallet = await DirectSecp256k1HdWallet.fromMnemonic(seed,{hdPaths: [path],prefix});
+        const clientOffline = await SigningStargateClient.offline(wallet,{registry:myRegistry});
+        const [account] = await wallet.getAccounts();
+
+        // myRegistry.register("/gamm.swap-exact-amount-in", osmosis.v1beta1.MsgSwapExactAmountIn);
+
+        let {msg,from,fee,memo} = parse_legacy_tx_format(jsonTx)
+        if(from !== account.address){
+            console.log("from:",from)
+            console.log("account: ",account.address)
+            console.error("invalid from address!")
+        }
+
+        let txData:any = {
+            accountNumber: account_number,
+            sequence: sequence,
+            chainId: chain_id,
+            msgs: [msg],
+            fee,
+            memo,
+        }
+        const signerData: any = {
+            accountNumber: txData.accountNumber,
+            sequence: txData.sequence,
+            chainId: txData.chainId,
+        };
+
+        console.log("{msg,from,fee}: ",{msg,from,fee})
+        console.log("MSG: ",JSON.stringify(msg))
+        const txRaw = await clientOffline.sign(
+            from,
+            txData.msgs,
+            txData.fee,
+            txData.memo,
+            signerData,
+        );
+
+        console.log("txRaw: ",txRaw)
+        // @ts-ignore
+        let body = txRaw.bodyBytes.toString("base64")
+        // @ts-ignore
+        let authInfoBytes = txRaw.authInfoBytes.toString("base64")
+        // @ts-ignore
+        let signatures = [new Buffer(txRaw.signatures[0]).toString("base64")]
+
+        let output = {
+            // @ts-ignore
+            serialized:TxRaw.encode(txRaw).finish().toString("base64"),
+            body,
+            authInfoBytes,
+            signatures
+        }
+        console.log("output: ",output)
+        // @ts-ignore
+        return output
+    } catch (e) {
+        console.error(tag, "e: ", e);
+        throw Error(e)
+    }
+}
+
+function makeCosmoshubPath(a: number): HdPath {
+    return [
+        Slip10RawIndex.hardened(44),
+        Slip10RawIndex.hardened(118),
+        Slip10RawIndex.hardened(0),
+        Slip10RawIndex.normal(0),
+        Slip10RawIndex.normal(a),
+    ];
+}
+
+const parse_legacy_tx_format = function(jsonTx:any){
+    try{
+        let txType = jsonTx.msg[0].type
+        if(!txType) throw Error("Invalid jsonTx input")
+        if(jsonTx.msg[1]) throw Error("multiple msgs not supported!")
+
+        let msg
+        let from
+        let fee
+        let memo
+        //switch for each tx type supported
+        switch (txType) {
+            case 'cosmos-sdk/MsgSend':
+                if(!jsonTx.msg[0].value.from_address) throw Error("Missing from_address in msg[0]")
+                if(!jsonTx.msg[0].value.to_address) throw Error("Missing to_address in msg[0]")
+                if(!jsonTx.msg[0].value.amount[0].amount) throw Error("Missing amount in msg[0] value.amount[0]")
+                from = jsonTx.msg[0].value.from_address
+                memo = jsonTx.memo
+                const msgSend: any = {
+                    fromAddress: jsonTx.msg[0].value.from_address,
+                    toAddress: jsonTx.msg[0].value.to_address,
+                    amount: coins(parseInt(jsonTx.msg[0].value.amount[0].amount), jsonTx.msg[0].value.amount[0].denom),
+                };
+                msg = {
+                    typeUrl: "/cosmos.bank.v1beta1.MsgSend",
+                    value: msgSend,
+                };
+                fee = {
+                    amount: coins(parseInt(jsonTx.fee.amount[0].amount), jsonTx.fee.amount[0].denom),
+                    gas: jsonTx.fee.gas,
+                };
+                break;
+            case 'cosmos-sdk/MsgDelegate':
+                if(!jsonTx.msg[0].value.delegator_address) throw Error("Missing delegator_address in msg[0]")
+                if(!jsonTx.msg[0].value.validator_address) throw Error("Missing validator_address in msg[0]")
+                if(!jsonTx.msg[0].value.amount.amount) throw Error("Missing amount in msg[0] value.amount[0]")
+                from = jsonTx.msg[0].value.delegator_address
+                memo = jsonTx.memo
+                const msgDelegate: any = {
+                    delegatorAddress: jsonTx.msg[0].value.delegator_address,
+                    validatorAddress: jsonTx.msg[0].value.validator_address,
+                    amount: coins(parseInt(jsonTx.msg[0].value.amount.amount), jsonTx.msg[0].value.amount.denom)[0],
+                };
+                msg = {
+                    typeUrl: "/cosmos.staking.v1beta1.MsgDelegate",
+                    value: msgDelegate,
+                };
+                fee = {
+                    amount: coins(parseInt(jsonTx.fee.amount[0].amount), jsonTx.fee.amount[0].denom),
+                    gas: jsonTx.fee.gas,
+                };
+                break;
+            case 'cosmos-sdk/MsgUndelegate':
+                if(!jsonTx.msg[0].value.delegator_address) throw Error("Missing delegator_address in msg[0]")
+                if(!jsonTx.msg[0].value.validator_address) throw Error("Missing validator_address in msg[0]")
+                if(!jsonTx.msg[0].value.amount.amount) throw Error("Missing amount in msg[0] value.amount[0]")
+                from = jsonTx.msg[0].value.delegator_address
+                memo = jsonTx.memo
+                const msgUnDelegate: any = {
+                    delegatorAddress: jsonTx.msg[0].value.delegator_address,
+                    validatorAddress: jsonTx.msg[0].value.validator_address,
+                    amount: coins(parseInt(jsonTx.msg[0].value.amount.amount), jsonTx.msg[0].value.amount.denom)[0],
+                };
+                msg = {
+                    typeUrl: "/cosmos.staking.v1beta1.MsgUndelegate",
+                    value: msgUnDelegate,
+                };
+                fee = {
+                    amount: coins(parseInt(jsonTx.fee.amount[0].amount), jsonTx.fee.amount[0].denom),
+                    gas: jsonTx.fee.gas,
+                };
+                break;
+            case 'cosmos-sdk/MsgBeginRedelegate':
+                if(!jsonTx.msg[0].value.delegator_address) throw Error("Missing delegator_address in msg[0]")
+                if(!jsonTx.msg[0].value.validator_src_address) throw Error("Missing validator_src_address in msg[0]")
+                if(!jsonTx.msg[0].value.validator_dst_address) throw Error("Missing validator_dst_address in msg[0]")
+                if(!jsonTx.msg[0].value.amount.amount) throw Error("Missing amount in msg[0] value.amount[0]")
+                from = jsonTx.msg[0].value.delegator_address
+                memo = jsonTx.memo
+                const msgReDelegate: any = {
+                    delegatorAddress: jsonTx.msg[0].value.delegator_address,
+                    validatorSrcAddress: jsonTx.msg[0].value.validator_src_address,
+                    validatorDstAddress: jsonTx.msg[0].value.validator_dst_address,
+                    amount: coins(parseInt(jsonTx.msg[0].value.amount.amount), jsonTx.msg[0].value.amount.denom)[0],
+                };
+                msg = {
+                    typeUrl: "/cosmos.staking.v1beta1.MsgBeginRedelegate",
+                    value: msgReDelegate,
+                };
+                fee = {
+                    amount: coins(parseInt(jsonTx.fee.amount[0].amount), jsonTx.fee.amount[0].denom),
+                    gas: jsonTx.fee.gas,
+                };
+                break;
+            case 'cosmos-sdk/MsgWithdrawDelegatorReward':
+                if(!jsonTx.msg[0].value.delegator_address) throw Error("Missing delegator_address in msg[0]")
+                if(!jsonTx.msg[0].value.validator_address) throw Error("Missing validator_address in msg[0]")
+                if(!jsonTx.msg[0].value.amount.amount) throw Error("Missing amount in msg[0] value.amount[0]")
+                from = jsonTx.msg[0].value.delegator_address
+                memo = jsonTx.memo
+                const msgRewards: any = {
+                    delegatorAddress: jsonTx.msg[0].value.delegator_address,
+                    validatorAddress: jsonTx.msg[0].value.validator_address,
+                    amount: coins(parseInt(jsonTx.msg[0].value.amount.amount), jsonTx.msg[0].value.amount.denom)[0],
+                };
+                msg = {
+                    typeUrl: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
+                    value: msgRewards,
+                };
+                fee = {
+                    amount: coins(parseInt(jsonTx.fee.amount[0].amount), jsonTx.fee.amount[0].denom),
+                    gas: jsonTx.fee.gas,
+                };
+                break;
+            case 'cosmos-sdk/MsgTransfer':
+                if(!jsonTx.msg[0].value.receiver) throw Error("Missing receiver in msg[0]")
+                if(!jsonTx.msg[0].value.sender) throw Error("Missing sender in msg[0]")
+                if(!jsonTx.msg[0].value.source_channel) throw Error("Missing source_channel in msg[0]")
+                if(!jsonTx.msg[0].value.source_port) throw Error("Missing source_port in msg[0]")
+                if(!jsonTx.msg[0].value.timeout_height.revision_height) throw Error("Missing revision_height in msg[0] value.timeout_height")
+                if(!jsonTx.msg[0].value.token.amount) throw Error("Missing amount in msg[0] value.token[0]")
+                const msgTransfer: any = {
+                    receiver:jsonTx.msg[0].value.receiver,
+                    sender:jsonTx.msg[0].value.sender,
+                    sourceChannel:jsonTx.msg[0].value.source_channel,
+                    sourcePort:jsonTx.msg[0].value.source_port,
+                    token:jsonTx.msg[0].value.source_port,
+                    timeoutHeight:jsonTx.msg[0].value.timeout_height.revision_height,
+                    timeoutTimestamp:jsonTx.msg[0].value.source_port,
+                };
+                msg = {
+                    typeUrl: "/ibc.applications.transfer.v1.MsgTransfer",
+                    value: msgTransfer,
+                };
+                fee = {
+                    amount: coins(parseInt(jsonTx.fee.amount[0].amount), jsonTx.fee.amount[0].denom),
+                    gas: jsonTx.fee.gas,
+                };
+                break;
+            case 'osmosis/gamm/swap-exact-amount-in':
+                //TODO
+                const msgSwap: any = {
+
+                };
+                msg = {
+                    typeUrl: "/gamm.swap-exact-amount-in",
+                    value: msgRewards,
+                };
+                fee = {
+                    amount: coins(parseInt(jsonTx.fee.amount[0].amount), jsonTx.fee.amount[0].denom),
+                    gas: jsonTx.fee.gas,
+                };
+                break;
+            default:
+                throw Error("Unhandled tx type! type: "+txType)
+        }
+        if(!from) throw Error("Failed to parse from address!")
+        if(!msg) throw Error("Failed to parse msg!")
+        if(!fee) throw Error("Failed to parse fee!")
+        return {msg,from,fee,memo}
+    }catch(e){
+        throw Error(e)
+    }
+}
