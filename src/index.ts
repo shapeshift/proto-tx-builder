962b16290f89ce27495d7e8cccae2f8bea006ece
MrNerdHair <mrnerdhair@gmail.com>
2022-06-08 23:44:53 -0400

    fix: don't skip "0" amounts, it breaks some reference txes
    The previous behavior prevented the reference TX serializations for
    osmosis lp-add and undelegate from matching the on-chain data. Note
    that osmosis lp-stake didn't match on-chain data before, and it still
    doesn't, both due to a serialization bug and because the reference TX
    doesn't use SIGN_MODE_DIRECT.

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ const scrubCoin = (x: Coin) => {
 
   return coin(x.amount, x.denom)
 }
-const scrubCoins = (x: Coin[]) => x.filter(c => c.amount && c.amount !== "0").map(scrubCoin)
+const scrubCoins = (x: Coin[]) => x.filter(c => c.amount).map(scrubCoin)
 
 type Route = { poolId: unknown; tokenOutDenom: unknown }
 const scrubRoute = (x: Route) => {
